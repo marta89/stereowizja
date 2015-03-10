150a159ddc4f44fd1b626c9a23f7b1d04af5f025
Wojciech Bieganski <wojtekbe@gmail.com>
2015-03-10 11:49:30 +0000

    Window title changed

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace cv;
 
 int main(int argc, char** argv) {
 	Mat inputImage = imread(argv[1]);
-	imshow("Input Image", inputImage);
+	imshow("Obrazek", inputImage);
 
 	waitKey(0);
 	return 0;
