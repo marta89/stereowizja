36e3264b28101dab194098bf5855fc6556489edf
wojtekbe <wojtekbe@gmail.com>
2015-03-23 21:00:39 +0100

    displaying image from /dev/video0

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <opencv2/opencv.hpp>
+
 using namespace cv;
 
-int main(int argc, char** argv) {
-	Mat inputImage = imread(argv[1]);
-	imshow("Obrazek", inputImage);
+int main(int argc, char** argv)
+{
+    VideoCapture cap(0);
+	Mat frame;
 
-	waitKey(0);
+	if(!cap.isOpened())
+		return -1;
+
+	while(1)  {
+        cap >> frame;
+        imshow("Image from /dev/video0", frame);
+        if(waitKey(30) >= 0) break;
+    }
+
 	return 0;
 }
